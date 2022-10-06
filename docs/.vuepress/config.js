efca6622c57c5faa0aa6355d16b3cb229be6d82a
Zhangchenhao <353060639@qq.com>
2022-10-06 15:39:00 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,6 +6,23 @@ module.exports = {
   theme: 'vdoing', // 使用npm包主题
   title: "vdoing-template",
   description: 'vdoing博客主题模板',
+  // vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "58152abb02c3be52cf27",
+        clientSecret: "236ab8bcb76ff196d520b0c7bfbac45182108344",
+        owner: "Zhangchenhao",
+        repo: "blog-template",
+      },
+    ],
+  ],
   base: '/vdoing-template/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
@@ -15,20 +32,4 @@ module.exports = {
   plugins,
   themeConfig,
 }
-// vssue 评论插件
-plugins: [
-  [
-    "vuepress-plugin-vssue-global",
-    {
-      platform: "github",
-      title: "[Comment]<%- frontmatter.title %>",
-      needComments: true,
-      // 其他的 Vssue 配置
-      autoCreateIssue: true,
-      clientId: "58152abb02c3be52cf27",
-      clientSecret: "236ab8bcb76ff196d520b0c7bfbac45182108344",
-      owner: "Zhangchenhao",
-      repo: "blog-template",
-    },
-  ],
-],
+
